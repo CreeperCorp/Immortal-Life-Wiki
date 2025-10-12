b442429e25df1d3b98d3ad6895a7389d724ddaa4
CreepSplotion <dekaiyondavis8@gmail.com>
2025-10-12 00:52:10 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,6 @@
+print('Only Season 9 is included right now')
+ifLore = input('Do you want to see Season 9 lore?(yes/no): ')
+
+if (ifLore = 'yes'):
+  print("On day 1, CreepYT got banned after joke betraying, and ECRACreepSplotion got the dragon egg. CreepYT got banned again on day 3. Creep made the Resin Mound, Ancient Debri Pond, and Temperature Graph Spawn. CreepYT actually left BloodShots and is going rogue.")
+  
