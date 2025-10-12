3796b8560d6ccc643185a3185bb82252dcc33b6d
ECRAEnnard <ennardcreep@gmail.com>
2025-10-12 12:43:09 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,3 +4,19 @@ ifLore = input('Do you want to see Season 9 lore?(yes/no): ')
 if (ifLore = 'yes'):
   print("On day 1, CreepYT got banned after joke betraying, and ECRACreepSplotion got the dragon egg. CreepYT got banned again on day 3. Creep made the Resin Mound, Ancient Debri Pond, and Temperature Graph Spawn. CreepYT actually left BloodShots and is going rogue.")
   
+ifVideos = input('Do you want to see Season 9 videos in chronological order?(yes/no): ')
+
+if (ifVideos = 'yes'):
+  print('CreepSplotion: The First Day')
+  print('ReapSplotion: A New Season')
+  print('Ennard: WTF')
+  print('CreepYT: 5 Minutes')
+  print('CreepSplotion: The Race For The Dragon Egg')
+  print("CreepSplotion: It Didn't Work")
+  print('CreepYT: REVIVED')
+  print('CreepYT: The Nether')
+  print('Ennard: Peace')
+  print('Division: Peace')
+  print('CreepYT: I Hate This')
+  print('CreepYT: So Called Friends')
+  print('CreepSplotion: 
