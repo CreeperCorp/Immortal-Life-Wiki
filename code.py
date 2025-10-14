cd3214ff876171abc9faffad07450b7df615a028
CreepSplotion <dekaiyondavis8@gmail.com>
2025-10-14 11:11:35 -0400

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ print('Only Season 9 is included right now')
 ifLore = input('Do you want to see Season 9 lore?(yes/no): ')
 
 if (ifLore = 'yes'):
-  print("On day 1, CreepYT got banned after joke betraying, and ECRACreepSplotion got the dragon egg. CreepYT got banned again on day 3. Creep made the Resin Mound, Ancient Debri Pond, and Temperature Graph Spawn. CreepYT actually left BloodShots and is going rogue.")
+  print("On day 1, CreepYT got banned after joke betraying, and ECRACreepSplotion got the dragon egg. CreepYT got banned again on day 3. Creep made the Resin Mound, Ancient Debri Pond, and Temperature Graph Spawn. CreepYT actually left BloodShots and is going rogue. CreepYT created an Orbital Strike Cannon.")
   
 ifVideos = input('Do you want to see Season 9 videos in chronological order?(yes/no): ')
 
@@ -20,3 +20,22 @@ if (ifVideos = 'yes'):
   print('CreepYT: I Hate This')
   print('CreepYT: So Called Friends')
   print('CreepSplotion: CreepSpawn')
+  print('CreepYT: My Evil Plan')
+  print('CreepYT: Succession')
+  print('CreepYT: The First Fire')
+  print('Division: CHOAS')
+  print('Ennard: Peace?')
+
+ifLive = input('Do you want to see Season 9 live streams in chronological order?(yes/no): ')
+
+if (ifLive = 'yes'):
+  print('CreepSplotion: The First Day | 10 hours')
+  print('ReapSplotion: The First Day | 6 hours')
+  print('Ennard: The First Day | 9 hours')
+  print('Division: The First Day | 27 hours')
+  print('CreepYT: The First Day | 8 minutes')
+  print('CreepYT: Banned Again | 3 hours')
+  print('CreepYT: Grind | 3 hours')
+  print('CreepYT: No | 3 hours')
+  print('CreepYT: Building & Destruction | 19 hours')
+  print("CreepYT: They're On | 3 hours')
